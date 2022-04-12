e5a65ef765d5b80c5571529e4b1b4e6f21eb84d8
Hunter Saunders <hjvs98@gmail.com>
2022-04-12 09:18:10 -0600

    calculatoranator

--- calc.js
+++ calc.js
@@ -15,7 +15,6 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-
 {
 	console.log("mathsymbol", mathSymbol);
 	console.log("num1:", num1);
@@ -45,8 +44,6 @@ reader.question("What would you like to calculate?", function(input){
 	}
 
 	
-
-
 }
 	// This line closes the connection to the command line interface.
 	reader.close()
